d052b3e7f368e076d124d47790cc6ab753f06c37
uttej528 <96988310+uttej528@users.noreply.github.com>
2023-12-28 20:53:14 +0530

    Create even_odd.cpp

---
+++ c++/even_odd.cpp
@@ -0,0 +1,19 @@
+//Write a program to check whether a number is Even or Odd program
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"enter the value of n"<<endl;
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"Even Number"<<endl;
+        
+    }else{
+        cout<<"Odd Number"<<endl;
+    }
+    
+    return 0;
+}
