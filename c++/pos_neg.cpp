7eff2de88548b8d08656b2d2b9677ef57f854338
uttej528 <96988310+uttej528@users.noreply.github.com>
2023-12-28 20:45:13 +0530

    Add files via upload

---
+++ c++/pos_neg.cpp
@@ -0,0 +1,19 @@
+//1.Given an integer input, The objective is to write a code to Check if a Number is Positive orNegative
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"enter the value of n"<<endl;
+    cin>>num;
+    if(num>0)
+    {
+        cout<<"positive number"<<endl;
+        
+    }else{
+        cout<<"negative number"<<endl;
+    }
+    
+    return 0;
+}
