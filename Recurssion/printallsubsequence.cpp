97f54b1959329554a36592caab98c34d3ce12423
uttej528 <96988310+uttej528@users.noreply.github.com>
2024-01-07 18:36:16 +0530

    Create printallsubsequence.cpp

---
+++ Recurssion/printallsubsequence.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void subseq(int arr[],int index,int n,vector<vector<int>>&ans,vector<int>temp)
+{
+    if(index==n){
+    ans.push_back(temp);
+    return;
+    }
+    
+    //no
+      subseq(arr,index+1,n,ans,temp);
+    //yes
+    temp.push_back(arr[index]);
+    subseq(arr,index+1,n,ans,temp);
+    
+}
+
+
+
+int main()
+{   
+    int index=0;int n=3;
+    int arr[]={1,2,3};
+    vector<vector<int>> ans;
+    vector<int> temp;
+    
+    subseq(arr,index,n,ans,temp);
+    
+    for (int i = 0; i < ans.size(); i++) {
+    for (int j = 0; j < ans[i].size(); j++) {
+        cout << ans[i][j] << " ";
+    }
+    cout << endl;
+}
+
+    return 0;
+}
